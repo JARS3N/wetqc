be47e4ba5a3ebdf48bdfc57d5cfaaf427bf33f63
JARS3N <JARS3N@users.noreply.github.com>
2023-10-10 10:02:03 -0400

    Update generate_ggtitle.R

--- R/generate_ggtitle.R
+++ R/generate_ggtitle.R
@@ -1,15 +1,1 @@
-generate_ggtitle <- function(u) {
 
-  title1 <-  c(mean_CV = "the average CV of Samples",
-               cv_avg = "The %CV of sample means",
-               mean_avg = "The Mean of Sample Means")[
-                 sapply(c("mean_cv", "cv_avg", "mean_avg"),
-                        grepl,
-                        x = u)]
-
-
-  aspect2 <- c("pH", "O2", "Gain", "KSV")
-  title2 <- aspect2[sapply(aspect2, grepl, x = u)]
-
-  paste0(title1, " for ", title2)
-}
