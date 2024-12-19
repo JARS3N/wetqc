52374e1dbf9db23507b192ed33c65b188c1870d8
JARS3N <JARS3N@users.noreply.github.com>
2024-12-18 22:54:39 -0500

    Update upload.R

--- R/upload.R
+++ R/upload.R
@@ -1,15 +1,15 @@
 upload <- function(u) {
   require(RMySQL)
-
+  type <- substr(unique(u$Lout), 1, 1)
   my_db <- adminKraken::con_mysql()
   dbWriteTable(
     my_db,
-    name = wetqc::type(unique(u$type)),
+    name = wetqc::type(type),
     value = u,
     append = TRUE,
     overwrite = FALSE,
     row.names = FALSE
   )
   dbDisconnect(my_db)
-
+  
 }
