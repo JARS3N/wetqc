64a88104ff2177d823a09311722837fecaca331e
JARS3N <JARS3N@users.noreply.github.com>
2025-01-27 17:55:52 -0500

    Update server.R

--- R/server.R
+++ R/server.R
@@ -7,8 +7,20 @@ server <- function() {
   require(foam)
 
   shinyServer(function(input, output, session) {
+    
+    # Initialize variables
+    DATA <- NULL
+    sum_tbl <- NULL
 
     session$onSessionEnded(function() { print("OUT!"); stopApp() })
+
+    # Clear tables and data on purge
+    purge_data <- function() {
+      DATA <<- NULL
+      sum_tbl <<- NULL
+      output$foo <- DT::renderDataTable(data.frame())
+      output$foo2 <- DT::renderDataTable(data.frame())
+    }
 
     # Initialize outputs
     output$foo <- DT::renderDataTable(data.frame())
@@ -19,10 +31,14 @@ server <- function() {
     })
 
     observeEvent(input$goButton, {
+      if (!is.null(DATA)) {
+        output$MSG <- renderText("Please purge the previous data before processing a new set.")
+        return()
+      }
+
       output$MSG <- renderText("Ready")
       if (length(input$goButton$name) > 0) {
-        output$MSG <- renderText("Select Directory")
-        output$MSG <- renderText("Munging Data...")
+        output$MSG <- renderText("Processing new dataset...")
         
         # Process uploaded files
         fls <- input$goButton$datapath
@@ -33,20 +49,19 @@ server <- function() {
         print(procd[[1]])
         
         # Process foam objects into dataframes for kraken
-        DATA <- purrr::map(procd, wetqc::format_kraken)
-        DATA <- lapply(DATA, function(u) {
+        DATA <<- purrr::map(procd, wetqc::format_kraken)
+        DATA <<- lapply(DATA, function(u) {
           x <- names(u)
           names(u) <- gsub("pH_target", "Target", x)
           u
         })
-        head(DATA[[1]])
-        
+
         # Render the main table
         output$foo2 <- DT::renderDataTable(dplyr::bind_rows(DATA))
-        sum_tbl <- purrr::map_df(procd, sum_table_row) %>%
+        sum_tbl <<- purrr::map_df(procd, sum_table_row) %>%
           arrange(sn)
 
-        if (exists("sum_tbl")) {
+        if (!is.null(sum_tbl)) {
           sum_tbl$use <- TRUE
           output$foo <- DT::renderDataTable(
             sum_tbl,
@@ -56,58 +71,42 @@ server <- function() {
             rownames = FALSE
           )
         }
-
-        # Handle row selections
-        observeEvent(input$foo_rows_selected, {
-          sum_tbl$use <- TRUE
-          sum_tbl$use[input$foo_rows_selected] <- FALSE
-          output$foo <- DT::renderDataTable(
-            sum_tbl,
-            selection = list(selected = input$foo_rows_selected),
-            server = FALSE,
-            options = list(dom = "t", pageLength = nrow(sum_tbl)),
-            rownames = FALSE
-          )
-        })
-
-        # Deselect rows
-        observeEvent(input$desel, {
-          sum_tbl$use <- TRUE
-          output$foo <- DT::renderDataTable(
-            sum_tbl,
-            selection = list(selected = NULL),
-            server = FALSE,
-            options = list(dom = "t", pageLength = nrow(sum_tbl)),
-            rownames = FALSE
-          )
-        })
-
-        # Download handler
-        output$exprt <- downloadHandler(
-          filename = function() {
-            paste("data-", Sys.Date(), ".csv", sep = "")
-          },
-          content = function(file) {
-            OUT <- DATA %>%
-              wetqc::remove_deselected(., input$foo_rows_selected) %>%
-              dplyr::bind_rows() %>%
-              arrange(sn, Well)
-            write.csv(OUT, file, row.names = FALSE)
-          }
-        )
-
-        # Handle upload and clear data
-        observeEvent(input$upload, {
-          print("before upload")
-          wetqc::upload_all(remove_deselected(DATA, input$foo_rows_selected))
-          print("post upload")
-          
-          # Clear tables and data
-          output$foo <- DT::renderDataTable(data.frame())
-          output$foo2 <- DT::renderDataTable(data.frame())
-          DATA <- NULL
-        })
       }
+    })
+
+    # Handle purging of data
+    observeEvent(input$purge, {
+      purge_data()
+      output$MSG <- renderText("Data purged. Ready for new upload.")
+    })
+
+    # Download handler
+    output$exprt <- downloadHandler(
+      filename = function() {
+        paste("data-", Sys.Date(), ".csv", sep = "")
+      },
+      content = function(file) {
+        OUT <- DATA %>%
+          wetqc::remove_deselected(., input$foo_rows_selected) %>%
+          dplyr::bind_rows() %>%
+          arrange(sn, Well)
+        write.csv(OUT, file, row.names = FALSE)
+      }
+    )
+
+    # Handle upload and clear data
+    observeEvent(input$upload, {
+      if (is.null(DATA)) {
+        output$MSG <- renderText("No data to upload. Please process a dataset first.")
+        return()
+      }
+
+      print("Uploading data...")
+      wetqc::upload_all(remove_deselected(DATA, input$foo_rows_selected))
+      print("Upload complete.")
+      
+      purge_data()  # Clear data after upload
+      output$MSG <- renderText("Data uploaded and cleared. Ready for new dataset.")
     })
   })
 }
